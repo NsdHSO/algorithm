dc7cb8d73d7174adf0b3d5f0dcab2adc4062d7ef
NsdHSO <nechiforelsamuel@yahoo.com>
2022-01-07 20:27:51 +0200

    chore: euclidean algo
    first / last = rest , first%last = remainder
    
    BREAKING CHANGE: greatest(last, remainder)

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -25,12 +25,41 @@ def factorial(n):
 
 def fibonaci(n):
 	assert n >= 0 and int(n) == n, "The number must be positive and integer number"
-	if n in [0, 1]:
-		return 1 
+	if n in [ 0,1]:
+		return n 
 	else:
 		# print(n)
 		return fibonaci(n-1)+ fibonaci(n-2)
 
-print(fibonaci(6))
+# print(fibonaci(600))
 
+def findNumber(n):
+	assert n >= 0 and int(n) == n, "The number must be positive and interger number"
+	if n == 0:
+		return 0
+	else:
+		return findNumber(int(n/10))+ int(n%10)
+
+# print(findNumber(12))
+
+def powerNumber(base, exponent):
+	assert base>=0 and exponent >= 0 and int(exponent)== exponent, "The base or the exponent must be positive or integer number"
+	if exponent == 0:
+		return 1
+
+	if base == 1:
+		return base
+
+	
+	return powerNumber(base,int(exponent-1)) * base
+
+# print(powerNumber(2.2, 7)i)
+
+def greatestCommon(first, last):
+	if last == 0 :
+		return first
+
+	return greatestCommon(last, first % last)
+print(greatestCommon(-48.3,0))
+	 
 	
